314d6632e5e5386a0b9f0eca5cfe3b8c458ecf18
caboomboom1 <catalinapereyra@seattleacademy.org>
2025-12-11 19:54:51 +0000

    P1

---
+++ adventiure.py
@@ -0,0 +1,19 @@
+answer1 = input ("School is over - where do you want to go")
+if answer1 == "basketball game":
+
+ if answer2 == "yes":
+        if answer2 =="yes"
+        print("you get some popcorn")
+        if answer2 == "no":
+            print ("You find a seat with your friend")
+
+if answer1 == ("leave school")
+
+    answer3 = input ("do you want go directly home?")
+    if answer3 == "yes":
+        print ("")
+
+
+
+
+
